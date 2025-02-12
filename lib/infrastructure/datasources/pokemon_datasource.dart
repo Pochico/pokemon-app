7ccac2b6c1950911d615c60719731dcf0d56da8b
User <user@MacBookAir.home>
2025-02-12 18:13:06 +0100

    fixed pokemon detail

--- lib/infrastructure/datasources/pokemon_datasource.dart
+++ lib/infrastructure/datasources/pokemon_datasource.dart
@@ -25,41 +25,8 @@ class PokemonDataSource {
   Future<PokemonDetail> getPokemonDetail(String name) async {
     try {
       final response = await dio.get('pokemon/$name');
-      final pokemon = response.data;
-      print(pokemon); // Para depuración
 
-      return PokemonDetail(
-        id: pokemon['id'] ?? 0, // Si es null, asigna 0
-        name: pokemon['name'] ?? '',
-        baseExperience: pokemon['base_experience'] ?? 0,
-        height: pokemon['height'] ?? 0,
-        weight: pokemon['weight'] ?? 0,
-        isDefault: pokemon['is_default'] ?? false,
-        locationAreaEncounters: pokemon['location_area_encounters'] ?? '',
-        species: pokemon['species'] != null
-            ? PokemonSpecies.fromJson(pokemon['species'])
-            : PokemonSpecies(name: '', url: ''),
-        abilities: (pokemon['abilities'] as List?)
-                ?.map((ability) => PokemonAbility.fromJson(ability['ability']))
-                .toList() ??
-            [],
-        moves: (pokemon['moves'] as List?)
-                ?.map((move) => PokemonMove.fromJson(move['move']))
-                .toList() ??
-            [],
-        types: (pokemon['types'] as List?)
-                ?.map((type) => PokemonType.fromJson(type['type']))
-                .toList() ??
-            [],
-        sprites: pokemon['sprites'] != null
-            ? PokemonSprites.fromJson(pokemon['sprites'])
-            : PokemonSprites(
-                frontDefault: '',
-                frontShiny: '',
-                backDefault: '',
-                backShiny: '',
-              ),
-      );
+      return PokemonDetail.fromJson(response.data);
     } catch (e) {
       throw Exception('Error al cargar Pokémon: $e');
     }
